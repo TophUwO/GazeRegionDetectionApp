90cfcd79f790b7d866bda918583ce4e4f92251a2
TophUwO <tophuwo01@gmail.com>
2025-10-30 13:37:49 +0100

    Revert "initial commit + HW"
    This reverts commit 9ad01cee3e4dfc0b9926f03457e9a3d2673844ae.

--- static/scripts/session.js
+++
@@ -1,7 +0,0 @@
-const btnCreateSession = document.getElementById('btnCreateSession');
-
-btnCreateSession.addEventListener('click', () => {
-    btnCreateSession.textContent = 'Hello, world!';
-});
-
-
