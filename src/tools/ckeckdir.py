3e0e2eb733ff893174667454cb9284e3b037bba7
TophUwO <tophuwo01@gmail.com>
2026-05-11 16:15:24 +0200

    can now specify dir in checkdir.py

--- src/tools/ckeckdir.py
+++ src/tools/ckeckdir.py
@@ -1,8 +1,11 @@
-from os import listdir, path
+from sys import argv
+from os  import listdir, path
 
 
 if __name__ == '__main__':
-    for file in listdir('.'):
+    dir = argv[1]
+
+    for file in listdir(dir):
         if file.endswith('.jpg') and not path.exists(file.replace('img', 'lbl'.replace('.jpg', '.json'))):
             print(f'error: Image ""{file} has no corresponding label.')
 
