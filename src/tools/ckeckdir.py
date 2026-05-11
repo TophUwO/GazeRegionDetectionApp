afbfd795e702193c09c35e3680ead436061e4c64
TophUwO <tophuwo01@gmail.com>
2026-05-11 16:18:03 +0200

    fix bug with incorrect search dir

--- src/tools/ckeckdir.py
+++ src/tools/ckeckdir.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     dir = argv[1]
 
     for file in listdir(dir):
-        if file.endswith('.jpg') and not path.exists(file.replace('img', 'lbl'.replace('.jpg', '.json'))):
+        if file.endswith('.jpg') and not path.exists(dir + file.replace('img', 'lbl'.replace('.jpg', '.json'))):
             print(f'error: Image ""{file} has no corresponding label.')
 
 
